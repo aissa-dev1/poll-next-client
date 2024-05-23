746d76e4c0129ab699e26bfb0ef72cf26d8be892
aissa-dev-1 <aissabedr74@gmail.com>
2024-05-23 21:10:44 +0100

    Update users api

--- src/api/users-fetch.ts
+++ src/api/users-fetch.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ChangePasswordData, DeleteAccountData } from "./types";
 
 class UsersFetch {
   async findOneWithoutPolls(id: string) {
@@ -26,6 +27,77 @@ class UsersFetch {
     const axiosRes = await axios.get(
       `${import.meta.env.VITE_API_URL}/users/${token}/findone-with-token`,
       {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return axiosRes;
+  }
+
+  async changeName(id: string, fullName: string, token: string) {
+    const axiosRes = await axios.patch(
+      `${import.meta.env.VITE_API_URL}/users/change-name/${id}`,
+      {
+        fullName,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return axiosRes;
+  }
+
+  async changeAvatar(id: string, avatar: string, token: string) {
+    const axiosRes = await axios.patch(
+      `${import.meta.env.VITE_API_URL}/users/change-avatar/${id}`,
+      {
+        avatar,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return axiosRes;
+  }
+
+  async changeBio(id: string, bio: string, token: string) {
+    const axiosRes = await axios.patch(
+      `${import.meta.env.VITE_API_URL}/users/change-bio/${id}`,
+      {
+        bio,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return axiosRes;
+  }
+
+  async changePassword(id: string, data: ChangePasswordData, token: string) {
+    const axiosRes = await axios.patch(
+      `${import.meta.env.VITE_API_URL}/users/change-password/${id}`,
+      data,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return axiosRes;
+  }
+
+  async deleteOne(id: string, data: DeleteAccountData, token: string) {
+    const axiosRes = await axios.delete(
+      `${import.meta.env.VITE_API_URL}/users/${id}`,
+      {
+        data,
         headers: {
           Authorization: token,
         },
